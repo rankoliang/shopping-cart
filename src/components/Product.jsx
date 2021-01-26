f436e0fb1effcc28a16803af5507e8a4b38bc546
Ranko Liang <rankoliang@gmail.com>
2021-01-25 21:22:12 -0500

    Add 'Add to Cart' button and price

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,32 @@
 import PlaceholderImage from './PlaceholderImage';
 import faker from 'faker';
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+`;
 
 const Product = () => {
   return (
     <div className="column is-one-third">
-      <div className="card h-100">
+      <div className="card h-100 pb-6">
         <div className="card-image">
-          <figure class="image is-4by3">
-            <PlaceholderImage width={400} height={300} />
+          <figure className="image is-4by3">
+            <PlaceholderImage width={600} height={450} />
           </figure>
         </div>
-        <div class="card-content">
-          <h3 class="title is-4">{faker.commerce.productName(2)}</h3>
-          <p class="subtitle is-5">{faker.lorem.words(5)}</p>
+        <div className="card-content">
+          <h3 className="title is-4">{faker.commerce.productName(2)}</h3>
+          <h4 className="subtitle is-5">{faker.lorem.words(5)}</h4>
         </div>
+        <Footer className="card-footer">
+          <div className="card-footer-item">$ {faker.commerce.price()}</div>
+          <button className="card-footer-item button is-primary h-100">
+            Add to Cart
+          </button>
+        </Footer>
       </div>
     </div>
   );
